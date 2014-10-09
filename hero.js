6413cf752322596e16ca121d3b851b999a278d66
gorillatron <jornandretangen@gmail.com>
2014-10-09 15:21:42 +0200

    heal at hp < 80 and distance to health is 2 or less

--- hero.js
+++ hero.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
   The only function that is required in this file is the "move" function
 
@@ -9,8 +9,8 @@
 
   The "move" function must return "North", "South", "East", "West", or "Stay"
   (Anything else will be interpreted by the game as "Stay")
-  
-  The "move" function should accept two arguments that the website will be passing in: 
+
+  The "move" function should accept two arguments that the website will be passing in:
     - a "gameData" object which holds all information about the current state
       of the battle
 
@@ -91,12 +91,12 @@ var move = function(gameData, helpers) {
   });
   var distanceToHealthWell = healthWellStats.distance;
   var directionToHealthWell = healthWellStats.direction;
-  
+
 
   if (myHero.health < 40) {
     //Heal no matter what if low health
     return directionToHealthWell;
-  } else if (myHero.health < 100 && distanceToHealthWell === 1) {
+  } else if (myHero.health < 80 && distanceToHealthWell === 2) {
     //Heal if you aren't full health and are close to a health well already
     return directionToHealthWell;
   } else {
